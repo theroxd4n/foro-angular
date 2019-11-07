e23f0fa46755e9d191282f6223e59875ced83655
theroxd4n <theroxmine@hotmail.es>
2019-11-07 21:49:01 +0100

    Fixed imports

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, from } from 'rxjs';
+import { Observable } from 'rxjs';
 import { User } from '../models/user';
 
 import { global } from './global';
